f4b8fdd92f6fa1e68fb6419b7f2f4b1f8bbd2139
Kaj Bostrom <kajb@allenai.org>
2019-06-25 14:01:58 -0700

    debug

--- allennlp/models/bert_for_classification.py
+++ allennlp/models/bert_for_classification.py
@@ -126,7 +126,7 @@ class BertForClassification(Model):
             try:
                 loss = self._loss(logits, label.long().view(-1))
             except RuntimeError:
-                logging.error(f'label: {repr(label)}')
+                logging.error(f'logits.size(): {logits.size()}')
                 raise RuntimeError()
                 
             output_dict["loss"] = loss
