9cd45ed844003c8145cb3e58c2166f45b3683cf4
Kaj Bostrom <kajb@allenai.org>
2019-06-25 12:47:20 -0700

    import qr_classify to register it

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -37,3 +37,4 @@ from allennlp.data.dataset_readers.simple_language_modeling import SimpleLanguag
 from allennlp.data.dataset_readers.babi import BabiReader
 from allennlp.data.dataset_readers.copynet_seq2seq import CopyNetDatasetReader
 from allennlp.data.dataset_readers.text_classification_json import TextClassificationJsonReader
+from allennlp.data.dataset_readers.qr_classify import QRClassifyDatasetReader
